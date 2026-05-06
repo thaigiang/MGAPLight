e2555aa8bc09db676514e604324e07d408480e87
thaigiang <thaigiang.it@gmail.com>
2026-05-06 22:46:18 +0700

    Update Check CUDA.py

--- Environment creation/Check CUDA.py
+++ Environment creation/Check CUDA.py
@@ -14,12 +14,12 @@ env = parallel_env(
     route_file=r"E:\Work\FPT University\#Documents\PhD\Paper 4 - Multi Agents\map\4x4c1c2c1c2.rou.xml",
     use_gui=False,
     num_seconds=60,
-    fixed_ts=True,          # chạy fixed-time
+    fixed_ts=True,          # run fixed-time
     delta_time=5,
     yellow_time=3,
 )
 obs, _ = env.reset(seed=0)
-for _ in range(12):         # 12 bước x 5s = 60s
+for _ in range(12):         # 12 steps x 5s = 60s
     actions = {a: 0 for a in obs.keys()}  # action dummy
     obs, rew, term, trunc, info = env.step(actions)
 env.close()
